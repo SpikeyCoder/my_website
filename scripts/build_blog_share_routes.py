ce7bd47fbe1e39d8eb79eafbcaf7c505d7a10bda
Kevin Armstrong <kevinmarmstrong1990@gmail.com>
2026-03-22 13:56:46 -0700

    Prevent mobile overflow on blog article pages

--- scripts/build_blog_share_routes.py
+++ scripts/build_blog_share_routes.py
@@ -16,7 +16,7 @@ CANONICAL_ORIGIN = "https://kevinarmstrong.io"
 DEFAULT_OG_IMAGE_URL = f"{CANONICAL_ORIGIN}/apple-touch-icon.png"
 MAX_POSTS = 200
 GEN_MARKER = "<!-- GENERATED_BLOG_ROUTE -->"
-SCRIPT_VERSION = "20260322a"
+SCRIPT_VERSION = "20260322b"
 
 GOINGVEGAN_CLEAN_SLUGS = {
     "how-many-animals-does-going-vegan-save-per-year",
